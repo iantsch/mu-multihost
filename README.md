b72fd7da741a37bd9e8942344770695e0e22f2dc
Christian Tschugg <c.tschugg@mmc-agentur.at>
2018-01-27 23:35:09 +0100

    [TASK] Multihosts  - Settings page  - More than one domain for a single wordpress installation  - Optional theme and front page switch  - Adding installation guide to readme  - Adding version number

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # mu-multihost
-WordPress must-use plugin to access the same instance on multiple domains
+WordPress must-use plugin to access the same instance on multiple domains with optional theme switch and front page switch.
+
+## Installation
+
+You can install this must-use plugin two ways
+
+### Via Composer
+
+If you load your dependenies via Composer you can load this plugin with
+
+```sh
+$ php composer require iantsch/mu-multihost
+```
+
+### Via Download
+
+Download/fork this repository and copy the contents of this must-use plugin into `wp-content/mu-plugins/`.
+If you visit your Plugin section in the `wp-admin` area, you should be able to see a newly created category (Must use) plugins
+
---
+++ mu-multihost.php
@@ -0,0 +1,299 @@
+<?php
+/*
+ * Plugin Name: mu-multihost
+ * Plugin URI: https://github.com/iantsch/mu-multihost
+ * Description: WordPress must-use plugin to access the same instance on multiple domains with optional theme switch and front page switch.
+ * Version: 0.1.0
+ * Author: Christian Tschugg
+ * Author URI: http://mbt.wien
+ * Copyright: Christian Tschugg
+ * Text Domain: mbt
+*/
+
+namespace MBT {
+
+    define( __NAMESPACE__ . '\Multihost\DIR', __DIR__ . DIRECTORY_SEPARATOR );
+
+    class Multihost {
+
+        /**
+         * @var Multihost;
+         */
+        protected static $instance;
+
+        /**
+         * @var string
+         */
+        private static $host;
+
+        /**
+         * @var array
+         */
+        private $domains = array();
+
+        /**
+         * Singleton Constructor
+         *
+         * @return Multihost
+         */
+        static public function init() {
+            if (is_null(self::$instance)) {
+                self::$instance = new self;
+            }
+            return self::$instance;
+        }
+
+        /**
+         * Multihost constructor.
+         */
+        private function __construct() {
+            self::$host          = $_SERVER['SERVER_NAME'];
+            $this->registerDomains();
+            $this->registerHooks();
+        }
+
+        /**
+         * Register available domains from options table
+         */
+        private function registerDomains() {
+            $domains = maybe_unserialize(get_option('mu-multihost', ''));
+            if ($domains) {
+                foreach ($domains['hosts'] as $domain) {
+                    $this->addDomain($domain['domain'], $domain['theme'], $domain['front_page']);
+                }
+            }
+        }
+
+        /**
+         * Register a domain and its optional theme and front page
+         *
+         * @param string $domain
+         * @param string|null $theme
+         * @param int|null $frontPage
+         */
+        private function addDomain( $domain, $theme = null, $frontPage = null ) {
+            if (empty($domain)) {
+                return;
+            }
+            $this->domains[ $domain ] = array(
+                'theme' => $theme,
+                'page_on_front' => $frontPage,
+            );
+        }
+
+        /**
+         * Register WordPress Action and Filter Hooks
+         */
+        private function registerHooks() {
+            add_filter( 'option_siteurl', array( $this, 'getHostAsSiteurl' ) );
+            add_filter( 'option_home', array( $this, 'getHostAsSiteurl' ) );
+            add_filter( 'content_url', array( $this, 'getHostAsSiteurl' ) );
+            add_filter( 'plugins_url', array( $this, 'getHostAsSiteurl' ) );
+            add_filter( 'option_template', array( $this, 'getThemeByHost' ) );
+            add_filter( 'option_stylesheet', array( $this, 'getThemeByHost' ) );
+            add_filter( 'pre_option_show_on_front', array( $this, 'showOnFrontByHost' ) );
+            add_filter( 'pre_option_page_on_front', array( $this, 'pageOnFrontByHost' ) );
+            add_action( 'admin_init',  array( $this, 'registerSettings' ) );
+            add_action( 'admin_menu', array($this, 'registerMenu' ) );
+        }
+
+        /**
+         * Return the theme for the current host or default theme
+         *
+         * @param $theme
+         *
+         * @return string theme
+         */
+        public function getThemeByHost( $theme ) {
+            if ( array_key_exists( self::$host, $this->domains ) ) {
+                if ( empty( $this->domains[ self::$host ]['theme'] ) ) {
+                    return $theme;
+                }
+                return $this->domains[ self::$host ]['theme'];
+            }
+            return $theme;
+        }
+
+        /**
+         * Return the hosts domain as site url or default url
+         *
+         * @param string $value
+         *
+         * @return string site url
+         */
+        public function getHostAsSiteurl( $value ) {
+            if ( array_key_exists( self::$host, $this->domains ) ) {
+                return $this->buildUrl( $value, self::$host );
+            }
+            return $value;
+        }
+
+        /**
+         * Build Url from given $host
+         *
+         * @param $url
+         * @param $host
+         *
+         * @return string Url from given host
+         */
+        private function buildUrl( $url, $host ) {
+            $url_parts = parse_url( $url );
+            if ( is_array( $url_parts ) ) {
+                $url = ( isset( $url_parts['scheme'] ) ? $url_parts['scheme'] . '://' : '' );
+                $url .= ( isset( $url_parts['user'] ) ? $url_parts['user'] . ':' : '' );
+                $url .= ( isset( $url_parts['pass'] ) ? $url_parts['pass'] . '@' : '' );
+                $url .= ( isset( $url_parts['host'] ) ? $host : '' );
+                $url .= ( isset( $url_parts['port'] ) ? ':' . $url_parts['port'] : '' );
+                $url .= ( isset( $url_parts['path'] ) ? $url_parts['path'] : '' );
+                $url .= ( isset( $url_parts['query'] ) ? '?' . $url_parts['query'] : '' );
+                $url .= ( isset( $url_parts['fragment'] ) ? '#' . $url_parts['fragment'] : '' );
+            }
+            return $url;
+        }
+
+        /**
+         * What to show (page or home) on front for current host
+         *
+         * @param $option
+         *
+         * @return string
+         */
+        public function showOnFrontByHost($option) {
+            if ( array_key_exists( self::$host, $this->domains ) ) {
+                if ( empty( $this->domains[ self::$host ]['page_on_front'] ) ) {
+                    return $option;
+                }
+                return 'page';
+            }
+            return $option;
+        }
+
+        /**
+         * Page on front for current host
+         *
+         * @param $post_id
+         *
+         * @return int post_id of page to display as front page
+         */
+        public function pageOnFrontByHost($post_id) {
+            if ( array_key_exists( self::$host, $this->domains ) ) {
+                if ( !empty( $this->domains[ self::$host ]['page_on_front'] ) ) {
+                    return $post_id;
+                }
+                return $this->domains[ self::$host ]['page_on_front'];
+            }
+            return $post_id;
+        }
+
+        public function registerSettings() {
+            register_setting( 'mu-multihost', 'mu-multihost' );
+            add_settings_section(
+                'mu-multihost',
+                __( 'Settings', 'mbt' ),
+                array($this, 'renderSettings'),
+                'mu-multihost'
+            );
+
+            // register a new field in the "wporg_section_developers" section, inside the "wporg" page
+            add_settings_field(
+                'hosts',
+                __( 'Hosts', 'mbt' ),
+                array($this, 'renderField'),
+                'mu-multihost',
+                'mu-multihost',
+                array(
+                    'label_for' => 'hosts'
+                )
+            );
+        }
+
+        public function renderSettings($args) {
+            echo "<p id='".esc_attr( $args['id'] )."'>".esc_html__( 'Enter your hosts, themes and front pages for multi-domain access', 'mbt' )."</p>";
+        }
+
+        public function renderField($args) {
+            $options = maybe_unserialize(get_option( 'mu-multihost', '' ));?>
+            <style>
+                .host {
+                    display: flex;
+                }
+            </style>
+            <div class="hosts">
+                <?php if ($options):
+                    foreach ($options[$args['label_for']] as $i => $option):?>
+                        <div class="host">
+                            <input type="text" name="mu-multihost[<?php echo esc_attr( $args['label_for'] ); ?>][<?php echo $i; ?>][domain]" placeholder="<?php _e('Domain','mbt');?>" value="<?php echo $option['domain'];?>">
+                            <input type="text" name="mu-multihost[<?php echo esc_attr( $args['label_for'] ); ?>][<?php echo $i; ?>][theme]" placeholder="<?php _e('Theme','mbt');?>" value="<?php echo $option['theme'];?>">
+                            <input type="text" name="mu-multihost[<?php echo esc_attr( $args['label_for'] ); ?>][<?php echo $i; ?>][front_page]" placeholder="<?php _e('Front Page','mbt');?>" value="<?php echo $option['front_page'];?>">
+                            <button class="button" data-mbt="remove-host">
+                                <?php _e('&times;');?>
+                            </button>
+                        </div>
+                    <?php endforeach;
+                endif;?>
+            </div>
+            <button class="button" data-mbt="add-host">
+                <?php _e('Add Host');?>
+            </button>
+            <script type="text/html" id="mbt-template-host">
+                <div class="host">
+                    <input type="text" name="mu-multihost[<?php echo esc_attr( $args['label_for'] ); ?>][###][domain]" placeholder="<?php _e('Domain','mbt');?>">
+                    <input type="text" name="mu-multihost[<?php echo esc_attr( $args['label_for'] ); ?>][###][theme]" placeholder="<?php _e('Theme','mbt');?>">
+                    <input type="text" name="mu-multihost[<?php echo esc_attr( $args['label_for'] ); ?>][###][front_page]" placeholder="<?php _e('Front Page','mbt');?>">
+                    <button class="button" data-mbt="remove-host">
+                        <?php _e('&times;');?>
+                    </button>
+                </div>
+            </script>
+            <script>
+                (function($) {
+                    $('body').on('click', "[data-mbt='add-host']", function(e) {
+                        e.preventDefault();
+                        var $hosts = $(this).prev(),
+                            template = $('#mbt-template-host').text();
+                        console.log($hosts, template);
+                        $hosts.append($(template.split('###').join($hosts.children().length)));
+                    }).on('click', "[data-mbt='remove-host']", function(e) {
+                        e.preventDefault();
+                        $(this).parent().remove();
+                    });
+                })( jQuery );
+            </script>
+            <?php
+        }
+
+        public function registerMenu() {
+            add_options_page(__('Multihost', 'mbt'),__('Multihost','mbt'),'manage_options','mu-multihost', array($this, 'renderSettingsPage'));
+        }
+
+        public function renderSettingsPage() {
+            if ( ! current_user_can( 'manage_options' ) ) {
+                return;
+            }
+
+            if ( isset( $_GET['settings-updated'] ) ) {
+                add_settings_error( 'mbt_messages', 'mbt_message', __( 'Hosts saved', 'mbt' ), 'updated' );
+            }
+            settings_errors( 'mbt_messages' );
+            ?>
+            <div class="wrap">
+                <h1><?php echo esc_html( get_admin_page_title() ); ?></h1>
+                <form action="options.php" method="post">
+                    <?php
+                    settings_fields( 'mu-multihost' );
+                    do_settings_sections( 'mu-multihost' );
+                    submit_button( __('Save','mbt') );
+                    ?>
+                </form>
+            </div>
+            <?php
+        }
+    }
+}
+
+namespace {
+
+    use MBT\Multihost;
+
+    Multihost::init();
+}
